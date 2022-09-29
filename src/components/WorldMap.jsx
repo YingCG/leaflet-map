ab0b016e69f8d5abeeacdd70888000e40618ee1b
Ying Chiu Goh <YingG@whiteclife.ac.nz>
2022-09-29 16:44:11 +1300

    add some click effect that is not working

--- src/components/WorldMap.jsx
+++ src/components/WorldMap.jsx
@@ -1,37 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { MapContainer, GeoJSON, Marker, Popup, Tooltip } from 'react-leaflet'
 import mapData from '../data/countries.json'
 import "leaflet/dist/leaflet.css"
 import "./WorldMap.css"
 
 function WorldMap() {
+    const [message, setMessage] = useState()
 
     // console.log(mapData);
 
     const mapStyle = { 
         fillColor: 'black',
-        color: '#A7FFE4',
+        color: '#F07DEA',
+        // color: '#A7FFE4',
         fillOpacity: 0.2,
-        weight: 0.5,
+        weight: 1,
         dashArray: 1
     }
 
-    const onEachCountry = (country, info) => {
+    // const showMessage = (event) => {
+    //     event.target.value = setMessage(
+    //         {idea: 'new idea!'}
+    //     )
+    // }
+
+    const changeCountryInfo = (country, info) => {
         const countryName = country.properties.ADMIN
-        const population = '1000'
         console.log(country);
-        info.bindPopup(countryName + "Population: " + population)
+        info.bindPopup(countryName)
+
+        info.on({
+            // click: (event) => {console.log(idea)},
+            click: (event) => {
+                const info = event.target.setMessage(
+                    'new idea!'
+                    )
+                    info.bindPopup(info)
+            },
+            // mouseover:  (event) => {
+            //     event.target.setStyle({
+            //         fillColor : '#CDF0EA',
+            //         fillOpacity: 0.2,
+            //         idea: 'What are you thinking?'
+            //     })
+            //     console.log('i can relate!')
+            // }
+        })
     }
+
 
     return (
         <div className='dark-theme'>
             <h1 className='map-title'>My Map</h1>
             <MapContainer style={{ minHeight: '100vh' }} zoom={3} center={[15, 100]}>
-                <GeoJSON data={mapData.features} style={mapStyle} onEachFeature={onEachCountry}/>
-                <Marker position={[-36, 171]}>
-                    <Popup>Popup for Marker</Popup>
-                    <Tooltip>Tooltip for Marker</Tooltip>
-                </Marker>
+                <GeoJSON data={mapData.features} style={mapStyle} changeCountryInfo={changeCountryInfo}/>
             </MapContainer>
         </div>
     )
